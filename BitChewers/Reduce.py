033f5b86cf22e3dd0f365d909aa138b6954974a4
MathYourLife <daniel.robert.couture@gmail.com>
2013-05-04 22:50:45 -0400

    Adding Minimum reducer class

--- BitChewers/Reduce.py
+++ BitChewers/Reduce.py
@@ -5,14 +5,14 @@ then filtered, then mapped.
 
 from collections import defaultdict
 
-class Map:
+class BaseReduce:
     def reduce(self, data):
         pass
 
-    def finish(self, data):
+    def finish(self):
         pass
 
-class Count(Map):
+class Count(BaseReduce):
     """
     A basic counting class.  Track the number of occurances of a key.
     """
@@ -26,23 +26,49 @@ class Count(Map):
         """
         self.counts[data[self.key]] += 1
 
-class Max(Map):
+class Min(BaseReduce):
+    """
+    Determine the min value encoutered grouped by the label field.
+    """
+    def __init__(self, label, value):
+        self.label = label
+        self.value = value
+        self.min = {}
+
+    def reduce(self, data):
+        """
+        Reducing function to reduce the recorded min if found
+        """
+
+        if data[self.label] not in self.min:
+            self.min[data[self.label]] = data[self.value]
+            return
+
+        if data[self.value] < self.min[data[self.label]]:
+            self.min[data[self.label]] = data[self.value]
+
+class Max(BaseReduce):
     """
     Determine the max value encoutered grouped by the label field.
     """
     def __init__(self, label, value):
         self.label = label
         self.value = value
-        self.max = defaultdict(int)
+        self.max = {}
 
     def reduce(self, data):
         """
         Reducing function to bump up the recorded max if found
         """
+
+        if data[self.label] not in self.max:
+            self.max[data[self.label]] = data[self.value]
+            return
+
         if data[self.value] > self.max[data[self.label]]:
             self.max[data[self.label]] = data[self.value]
 
-class BasicStats(Map):
+class BasicStats(BaseReduce):
     """
     Limited stats that don't require a history to compute.
     """
